ce225812957bb0867adde545311ee4970e5edd84
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-20 17:45:27 +0200

    creato il footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,68 @@
+export default function Footer (){
+    return (<footer>
+        <section id="page-info">
+            <div className="row">
+                <div className="col-1-2">
+                    <div className="row">
+                        <div className="col-1-3">
+                            <h3>DC COMICS</h3>
+                            <p>Comics</p>
+                            <p>Games</p>
+                            <p>Video</p>
+                            <p>News</p>
+                            <p>Video</p>
+                            <p>Video</p>
+                            <h3>SHOP</h3>
+                            <p>Shop</p>
+                            <p>Shop</p>
+                            <p>Shop</p>
+                        </div>
+                        <div className="col-1-3">
+                            <h3>DC</h3>
+                            <p>Privacy</p>
+                            <p>Shop</p>
+                            <p>Contact</p>
+                            <p>Ratings</p>
+                             <p>Privacy</p>
+                            <p>Shop</p>
+                            <p>Contact</p>
+                            <p>Ratings</p>
+                             <p>Privacy</p>
+                            <p>Shop</p>
+                            <p>Contact</p>
+                        </div>
+                        <div className="col-1-3">
+                            <h3>SITES</h3>
+                            <p>Dc Kids</p>
+                            <p>Dc</p>
+                            <p>Dc Universe</p>
+                            <p>Dc Power</p>
+                        </div>
+                    </div>
+                </div>
+                 <div className="col-1-2">
+                    <img src="/dc-logo-bg.png" id="logo-bg" alt=""></img>
+                 </div>
+            </div>
+        </section>
+        <section id="page-follow">
+            <div className="row">
+                <div className="col-1-2">
+                    <p>SIGN-UP NOW!</p>
+                </div>
+                 <div className="col-1-2">
+                    <div className="card-title">
+                        <h3>FOLLOW US</h3>
+                    </div>
+                    <div className="card-logo">
+                        <img src="/footer-facebook.png" id="footer-logo" alt=""></img>
+                        <img src="/footer-twitter.png" alt=""></img>
+                        <img src="/footer-youtube.png" alt=""></img>
+                        <img src="/footer-pinterest.png" alt=""></img>
+                        <img src="/footer-periscope.png" alt=""></img>
+                    </div>
+                 </div>
+            </div>
+        </section>
+    </footer>)
+}
